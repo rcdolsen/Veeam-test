89eb50150472fc613c277b50dc173e6f725ec41c
rcdolsen <rcd_olsen@yahoo.com.br>
2024-02-16 22:15:52 +0000

    added an iteration to sincronize the source and replica folders and created a function to make logs

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import logging
 import os
+import shutil
 from pathlib import Path
+
+source_files = []
+replica_files = []
 
 
 def clear():
@@ -20,7 +24,7 @@ def folder_creator(folder):
         if input_path == '':
             input_path = Path(__file__).parent
 
-        folder_path = Path(input_path/folder)
+        folder_path = Path(input_path) / folder
         try:
             folder_path.mkdir(parents=True, exist_ok=True)
         except (OSError):
@@ -35,7 +39,54 @@ def folder_creator(folder):
           f'{folder_path.parent.resolve()}'
           )
 
+    return folder_path
 
-folder_creator('source')
 
-folder_creator('replica')
+source_folder = folder_creator('source')
+
+replica_folder = folder_creator('replica')
+
+logs_folder = folder_creator('logs')
+
+
+def make_log(operation):
+    logging.basicConfig(
+        filename=logs_folder.joinpath('operations.log'),
+        filemode='a',
+        format='%(levelname)s - %(asctime)s - %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    logging.info(f'a file was {operation}')
+
+
+for source_file in source_folder.glob('*'):
+    source_files.append(source_file.name)
+
+for replica_file in replica_folder.glob('*'):
+    replica_files.append(replica_file.name)
+
+for source_file in source_folder.glob('*'):
+    if source_file.name in replica_files:
+        continue
+    else:
+        if source_file.is_file():
+            shutil.copy(source_file, replica_folder)
+        elif source_file.is_dir():
+            shutil.copytree(source_file, replica_folder/source_file.name)
+
+
+for replica_file in replica_folder.glob('*'):
+    if replica_file.name in source_files:
+        continue
+    else:
+        if replica_file.is_file():
+            Path.unlink(replica_file)
+        elif replica_file.is_dir():
+            shutil.rmtree(replica_file)
+
+make_log('erased')
+make_log('copied')
+make_log('created')
+
+# sinc = input('input the sincronization interval')
